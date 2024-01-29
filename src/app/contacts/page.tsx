ad81745436229e3415a731d0bb64896c27820866
unknown <mwangiarthur777@gmail.com>
2024-01-29 13:28:13 +0300

    new commit

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import styles from "./contact.module.css";
 import Link from "next/link";
 import CONTACTIMG from "../../../public/images/contact.png";
 import Image from "next/image";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const page = () => {
+  const { state } = useContext(ThemeContext);
   return (
-    <main className="w-full flex flex-col items-center justify-center">
+    <main
+      className={
+        state.isDarkMode == true
+          ? "w-full flex flex-col items-center justify-center bg-black"
+          : "w-full flex flex-col items-center justify-center"
+      }
+    >
       <section className="container flex items-center justify-between  h-[95vh]">
         <div className={`${styles.contact__left}`}>
-          <p className="text-[13px]  mb-[3px] text-gray-900/20">
+          <p
+            className={
+              state.isDarkMode == true
+                ? "text-[13px]  mb-[3px] text-white/50"
+                : "text-[13px]  mb-[3px] text-gray-900/20"
+            }
+          >
             Contact details
           </p>
           <h1 className="text-transparent  bg-clip-text bg-gradient-to-r from-blue-400 to-pink-600  text-3xl font-bold ">
             We are social.<br></br>Reach us both online and offline.
           </h1>
-          <p className="mt-6">
+          <p className={state.isDarkMode == true ? "mt-6 text-white" : "mt-6"}>
             <strong>
               <Link href="mailto:react777@gmail.com">
                 reactdev777@gmail.com:
@@ -23,14 +39,20 @@ const page = () => {
             </strong>
             For any inquiries and service proposal
           </p>
-          <p className="mt-2">
+          <p className={state.isDarkMode == true ? "mt-6 text-white" : "mt-6"}>
             <strong>
               <Link href="tel:+254742068914">Click here to call us:</Link>
             </strong>
             For any inquiries and service proposal or any technical consultaion
           </p>
           <div className="flex flex-col mt-7">
-            <p className="text-zinc-600">Visit our social media platform</p>
+            <p
+              className={
+                state.isDarkMode == true ? "text-white" : "text-zinc-600"
+              }
+            >
+              Visit our social media platform
+            </p>
             <div className="mt-2 flex flex-wrap gap-2">
               <Link
                 href="https://www.facebook.com/profile.php?id=100064319250841"
