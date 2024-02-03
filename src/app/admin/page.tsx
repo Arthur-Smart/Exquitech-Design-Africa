c57d89b75370e1e9a26f3a502c8713389ad78264
unknown <mwangiarthur777@gmail.com>
2024-02-03 10:14:29 +0300

    new commit

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -49,6 +49,34 @@ const page = () => {
           You have 2 requests
         </div>
       </section>
+      <div className="h-[2px] mt-3 container bg-zinc-200 py-9 rounded-md bg-gradient-to-r from-indigo-700 to-indigo-500"></div>
+      <section className="mt-4 container p">
+        <div>
+          <Image
+            src={USERIMAGE}
+            alt=""
+            width={100}
+            height={100}
+            className={styles.user__avatar__pic}
+          />
+          <p className="font-semibold text-lg mt-2">Joshua Maina</p>
+          <p className="font-medium text-zinc-700 ">joshua@gmail.com</p>
+          <p className="mt-2 text-[15px] text-zinc-500">
+            {" "}
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book
+          </p>
+          <div className="my-2">
+            <button className="py-[4px] px-5 bg-blue-500 rounded-md text-white">
+              Pending
+            </button>
+            <button className="py-[4px] px-5 bg-[tomato] ml-2 rounded-md text-white">
+              Delete
+            </button>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
